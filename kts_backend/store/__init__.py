db0d760b9efb2a81d0b73457429fd934ed98a3e4
serenityblood <miracle_5@bk.ru>
2023-09-20 20:46:50 +0300

    Presetup to separate bot and api logic

--- kts_backend/store/__init__.py
+++ kts_backend/store/__init__.py
@@ -1,5 +1,20 @@
+import typing
+
+from store.database.database import Database
+
+if typing.TYPE_CHECKING:
+    from web.app import Application
+
+
 class Store:
     def __init__(self, *args, **kwargs):
         from kts_backend.users.accessor import UserAccessor
 
         self.user = UserAccessor(self)
+
+
+def setup_store(app: "Application"):
+    app.database = Database(app)
+    app.on_startup.append(app.database.connect)
+    app.on_cleanup.append(app.database.disconnect)
+    app.store = Store(app)
--- kts_backend/web/app.py
+++ kts_backend/web/app.py
@@ -1,21 +1,40 @@
-from typing import Sequence, Callable
+import os
+from typing import Sequence, Callable, Optional
 
 from aiohttp.web import (
     Application as AiohttpApplication,
     View as AiohttpView,
     Request as AiohttpRequest,
 )
+
 from pyparsing import Optional
 
-
 from kts_backend import __appname__, __version__
+from kts_backend.store import setup_store, Store
+from kts_backend.store.database import Database
+from .config import setup_config, Config
+from .mw import setup_middlewares
+
 from .urls import register_urls
 
 
-__all__ = ("ApiApplication",)
+__all__ = ("Application", )
 
 
 class Application(AiohttpApplication):
-    config = None
-    store = None
-    database = None
+    config: Optional[Config] = None
+    store: Optional[Store] = None
+    database: Optional[Database] = None
+
+
+app = Application()
+
+
+def setup_app(config_path: str) -> Application:
+    setup_config(app, config_path)
+    setup_middlewares(app)
+    setup_store(app)
+    return app
+
+
+application = setup_app(os.path.join(os.path.dirname(__file__), "..", "..", "etc/config.yaml"))
